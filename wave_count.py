457d23a4a4a357f88c880c7b21deb4584a1742b6
THONTEPU MANIKANTA KUMAR <103179236+Kumar1199@users.noreply.github.com>
2022-06-04 14:16:13 +0530

    a=int(input()) b=[int(x) for x in input().split()] count=0 f = 1 for i in range(0,len(b)-2, 2):     if b[i]<b[i+1]:         if b[i+1]>b[i+2]:             count = count+1         else:             f = 0             break     elif b[i]>b[i+1]:         if b[i+1]<b[i+2]:             count = count+1         else:             f = 0             break print(count) if f == 1 else print(-1)

---
+++ wave_count.py
@@ -0,0 +1,18 @@
+a=int(input())
+b=[int(x) for x in input().split()]
+count=0
+f = 1
+for i in range(0,len(b)-2, 2):
+    if b[i]<b[i+1]:
+        if b[i+1]>b[i+2]:
+            count = count+1
+        else:
+            f = 0
+            break
+    elif b[i]>b[i+1]:
+        if b[i+1]<b[i+2]:
+            count = count+1
+        else:
+            f = 0
+            break
+print(count) if f == 1 else print(-1)
