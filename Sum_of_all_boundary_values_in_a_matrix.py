946a9033b97a0f210180f96ee1c8366b9f5a2815
THONTEPU MANIKANTA KUMAR <103179236+Kumar1199@users.noreply.github.com>
2022-09-04 10:19:13 +0530

    n,m=map(int,input().split()) mat=[] for i in range(n):     l=[int(x) for x in input().split()]     mat.append(l) c=0 for i in range(n):     for j in range(m):             c+=mat[i][j] for i in range(1,n-1):     for j in range(1,m-1):         c-=mat[i][j] print(c)

---
+++ Sum_of_all_boundary_values_in_a_matrix.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+mat=[]
+for i in range(n):
+    l=[int(x) for x in input().split()]
+    mat.append(l)
+c=0
+for i in range(n):
+    for j in range(m):
+            c+=mat[i][j]
+for i in range(1,n-1):
+    for j in range(1,m-1):
+        c-=mat[i][j]
+print(c)
