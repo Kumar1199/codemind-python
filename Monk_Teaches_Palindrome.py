9a76e63b12d4c56d41c0dbcfdbd29d9a34cb75aa
THONTEPU MANIKANTA KUMAR <103179236+Kumar1199@users.noreply.github.com>
2022-09-20 13:28:00 +0530

    for i in range(int(input())):     a=input()     if a==a[::-1] and len(a)%2==0:         print("YES EVEN")     elif a==a[::-1] and len(a)%2!=0:         print("YES ODD")     else:         print("NO")

---
+++ Monk_Teaches_Palindrome.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    a=input()
+    if a==a[::-1] and len(a)%2==0:
+        print("YES EVEN")
+    elif a==a[::-1] and len(a)%2!=0:
+        print("YES ODD")
+    else:
+        print("NO")
+        
