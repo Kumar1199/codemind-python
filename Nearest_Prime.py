8169c039757a306f91480414ff98aae897483e5f
THONTEPU MANIKANTA KUMAR <103179236+Kumar1199@users.noreply.github.com>
2022-09-22 12:59:31 +0530

    def prime(n):     if n==1:return False     for i in range(2,int(n**0.5)+1):         if n%i==0:return False     return True def pp(n):     while prime(n)==False:         n-=1     return n def np(n):     while prime(n)==False:         n+=1     return n for i in range(int(input())):     n=int(input())     x=pp(n)     y=np(n)     if n-x<=y-n:         print(x)     else:print(y)

---
+++ Nearest_Prime.py
@@ -0,0 +1,20 @@
+def prime(n):
+    if n==1:return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:return False
+    return True
+def pp(n):
+    while prime(n)==False:
+        n-=1
+    return n
+def np(n):
+    while prime(n)==False:
+        n+=1
+    return n
+for i in range(int(input())):
+    n=int(input())
+    x=pp(n)
+    y=np(n)
+    if n-x<=y-n:
+        print(x)
+    else:print(y)
