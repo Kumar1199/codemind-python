758381bb8d1a68dfeb6982e276b68f54c5728655
THONTEPU MANIKANTA KUMAR <103179236+Kumar1199@users.noreply.github.com>
2022-10-16 17:20:46 +0530

    n=int(input()) c=1 d=n-1 for i in range(1,n+1):     for j in range(d):         print(" ",end="")     for k in range(c):         print(i,end="")     c+=2     d-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,12 @@
 n=int(input())
-l=[]
+c=1
+d=n-1
 for i in range(1,n+1):
-    l.append(str(i))
-    print("".join(l))
+    for j in range(d):
+        print(" ",end="")
+    for k in range(c):
+        print(i,end="")
+    c+=2
+    d-=1
+    print()
+    
